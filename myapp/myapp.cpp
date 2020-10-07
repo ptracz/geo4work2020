91c087e4d877c9a87ac1dfc5ac939fff2f9f24e0
kmeg <ptiroat@gmail.com>
2020-10-07 20:48:05 +0200

    - Adding simple GDAL check function

--- myapp/myapp.cpp
+++ myapp/myapp.cpp
@@ -1,12 +1,33 @@
 #include "myapp.h"
 #include <iostream>
 
+#include <ogr\ogrsf_frmts\ogrsf_frmts.h>
+#include <gdal.h>
+
 using namespace std;
 
 int main()
 {
 	cout << "my frist app" << endl;
-
+	
+	SimleGDALCompilerChecker();
 
 	system("pause");
+	return 0;
+}
+
+// this function is checking if project is configured properly to use GDAL
+void SimleGDALCompilerChecker()
+{
+	GDALDriver *pDriver = nullptr;
+
+	OGRRegisterAll();
+
+	pDriver = GetGDALDriverManager()->GetDriverByName("ESRI Shapefile");
+
+	if(pDriver == nullptr)
+	{
+		cout << endl << "There is something wrong with project configuration for GDAL" << endl;
+	}
+	else cout << endl  << "Project is properly configured for GDAL. You rock!" << endl;
 }
